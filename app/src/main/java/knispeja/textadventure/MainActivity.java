55f0d4c20b9516aee4b33cc793fe63d7a02963c7
knispeja@rose-hulman.edu <knispeja@rose-hulman.edu>
2018-06-18 18:48:06 -0500

    Created simple scrolling text log with button to inject text

--- app/src/main/java/knispeja/textadventure/MainActivity.java
+++ app/src/main/java/knispeja/textadventure/MainActivity.java
@@ -1,48 +1,36 @@
 package knispeja.textadventure;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
-public class MainActivity extends AppCompatActivity {
+import knispeja.textadventure.knispeja.textadventure.ui.LimitedTextLog;
 
-    private View mContentView;
+public class MainActivity extends AppCompatActivity
+{
+    LimitedTextLog log;
+    int x = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        mContentView = findViewById(R.id.fullscreen_content);
+        this.log = new LimitedTextLog(this);
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        hide();
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
+    private final Runnable dummyButtonClicked = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            log.AddToLog("This is some long text intended to wrap around at least one line " + x++);
         }
+    };
 
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    public void onClickBtn(View v)
+    {
+        new Thread(dummyButtonClicked).start();
     }
 }
